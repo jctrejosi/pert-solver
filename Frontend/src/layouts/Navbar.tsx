ef8b9da35744b519c8317733dd0db0baf90192ad
Juan Carlos Trejos <juan.trejos@arandasoft.com>
2026-02-20 18:41:06 -0500

    Add link repo

--- Frontend/src/layouts/Navbar.tsx
+++ Frontend/src/layouts/Navbar.tsx
@@ -1,15 +1,17 @@
-import { AppBar, Typography } from "@mui/material";
+import { AppBar, Typography, Box, Link } from "@mui/material";
+import github from "../assets/github.svg";
 
 export const Navbar = () => {
   return (
     <AppBar
       position="static"
       sx={{
-        padding: "1.5rem ",
-        height: "2rem",
+        px: 3,
+        height: "4rem",
         display: "flex",
+        flexDirection: "row",
         alignItems: "center",
-        justifyContent: "center",
+        justifyContent: "space-between",
         backgroundColor: "#1e1e1e",
         color: "#fff",
       }}
@@ -17,6 +19,34 @@ export const Navbar = () => {
       <Typography variant="h6" sx={{ display: "flex", alignItems: "center" }}>
         PERT-Solver
       </Typography>
+
+      <Link
+        href="https://github.com/jctrejosi/pert-solver"
+        target="_blank"
+        rel="noopener"
+        underline="always"
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          gap: 1,
+          color: "#fff",
+          fontSize: "0.8rem",
+          textDecorationColor: "#fff",
+          "&:hover": {
+            color: "#fff",
+            textDecorationColor: "#fff",
+            opacity: 0.9,
+          },
+        }}
+      >
+        <Box
+          component="img"
+          src={github}
+          alt="GitHub"
+          sx={{ width: 20, height: 20 }}
+        />
+        Ver repositorio
+      </Link>
     </AppBar>
   );
 };
